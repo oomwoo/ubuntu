1774fc8670aa78b62ae54fa0929c853d180167a8
oomwoo <camera@ovsy.com>
2016-05-11 22:32:33 -0700

    First version that runs on Neon

--- trainbot.py
+++ trainbot.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
-# ----------------------------------------------------------------------------
-# Copyright 2015 Nervana Systems Inc.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Train robot to drive autonomously using Nervana Neon
+# See more at https://github.com/oomwoo/
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Copyright (C) 2016 oomwoo.com
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ----------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3.0
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License <http://www.gnu.org/licenses/> for details.
 """
-Small CIFAR10 based convolutional neural network. Showcases the use of cost
-scaling with the fp16 data format.
+Train robot to drive autonomously. Use a small convolutional neural network.
 """
 
-import numpy as np
-from neon.data import ArrayIterator, load_cifar10
+#import numpy as np
 from neon.initializers import Uniform
 from neon.layers import Affine, Conv, Pooling, GeneralizedCost
 from neon.models import Model
@@ -27,43 +24,49 @@ from neon.optimizers import GradientDescentMomentum
 from neon.transforms import Misclassification, Rectlin, Softmax, CrossEntropyMulti
 from neon.callbacks.callbacks import Callbacks
 from neon.util.argparser import NeonArgparser
+from neon.data.imageloader import ImageLoader
 
 # parse the command line arguments
 parser = NeonArgparser(__doc__)
 args = parser.parse_args()
 
 # hyperparameters
-if args.datatype in [np.float16]:
-    cost_scale = 10.
 num_epochs = args.epochs
 
-(X_train, y_train), (X_test, y_test), nclass = load_cifar10(path=args.data_dir)
+#(X_train, y_train), (X_test, y_test), nclass = load_cifar10(path=args.data_dir)
 
-train = ArrayIterator(X_train, y_train, nclass=nclass, lshape=(3, 32, 32))
-test = ArrayIterator(X_test, y_test, nclass=nclass, lshape=(3, 32, 32))
+#train = ArrayIterator(X_train, y_train, nclass=nclass, lshape=(3, 32, 32))
+#test = ArrayIterator(X_test, y_test, nclass=nclass, lshape=(3, 32, 32))
+
+train = ImageLoader(repo_dir='/home/ilia/nervana/data', set_name='train',
+                        inner_size=64,
+                        scale_range=0,  # Force scaling to match inner_size
+                        do_transforms=False,
+                        shuffle=True,
+                        contrast_range=(75,125))
+
+test = ImageLoader(repo_dir='/home/ilia/nervana/data', set_name='validation',
+                    inner_size=64,
+                    do_transforms=False,
+                    scale_range=0,  # Force scaling to match inner_size
+                    shuffle=True,
+                    contrast_range=(75, 125))
+
 
 init_uni = Uniform(low=-0.1, high=0.1)
-if args.datatype in [np.float32, np.float64]:
-    opt_gdm = GradientDescentMomentum(learning_rate=0.01,
-                                      momentum_coef=0.9,
-                                      stochastic_round=args.rounding)
-elif args.datatype in [np.float16]:
-    opt_gdm = GradientDescentMomentum(learning_rate=0.01/cost_scale,
-                                      momentum_coef=0.9,
-                                      stochastic_round=args.rounding)
+opt_gdm = GradientDescentMomentum(learning_rate=0.01,
+                                  momentum_coef=0.9,
+                                  stochastic_round=args.rounding)
 
 bn = True
 layers = [Conv((5, 5, 16), init=init_uni, activation=Rectlin(), batch_norm=bn),
           Pooling((2, 2)),
           Conv((5, 5, 32), init=init_uni, activation=Rectlin(), batch_norm=bn),
           Pooling((2, 2)),
-          Affine(nout=500, init=init_uni, activation=Rectlin(), batch_norm=bn),
-          Affine(nout=10, init=init_uni, activation=Softmax())]
+          Affine(nout=100, init=init_uni, activation=Rectlin(), batch_norm=bn),
+          Affine(nout=4, init=init_uni, activation=Softmax())]
 
-if args.datatype in [np.float32, np.float64]:
-    cost = GeneralizedCost(costfunc=CrossEntropyMulti())
-elif args.datatype in [np.float16]:
-    cost = GeneralizedCost(costfunc=CrossEntropyMulti(scale=cost_scale))
+cost = GeneralizedCost(costfunc=CrossEntropyMulti())
 
 mlp = Model(layers=layers)
 
